2d0fa395d06da98033ec37608e8fdc41c0f6965a
Sazi <mphosazi@gmail.com>
2026-05-29 13:14:40 +0200

    set login as the default page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return React.createElement(BrowserRouter, null,
     React.createElement(Routes, null,
+      React.createElement(Route, { path: '/', element: React.createElement(Navigate, { to: '/login', replace: true }) }),
       React.createElement(Route, { path: '/login', element: React.createElement(Login) }),
-      React.createElement(Route, { path: '/register', element: React.createElement(Login) })
+      React.createElement(Route, { path: '/register', element: React.createElement(Register) })
     )
   )
 }
